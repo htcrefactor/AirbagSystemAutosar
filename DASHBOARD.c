dd2472702ee93f978b367b850e30dde64e97d32c
Hyung-Taik Choi <htcrefactor@ajou.ac.kr>
2020-10-25 16:40:40 +0900

    Change type from  uint32 to uint32_t.

--- DASHBOARD.c
+++ DASHBOARD.c
@@ -8,7 +8,7 @@ int* uint32Unpacker(uint32 packed_data) {
 }
 
 // speed(0~2^8) | ocs_fl(0|1) | ocs_fr(0|1) | ocs_rl(0|1) | ocs_rr(0|1) | belt_fl(0|1) | belt_fr(0|1) | belt_rl(0|1) | belt_rr(0|1)
-uint32 uint32Packer(uint32 speed, uint32 ocs_fl, uint32 ocs_fr, uint32 ocs_rl, uint32 ocs_rr, uint32 belt_fl, uint32 belt_fr, uint32 belt_rl, uint32 belt_rr) {
-    uint32 res = speed << 24 | ocs_fl << 7 | ocs_fr << 6 | ocs_rl << 5 | ocs_rr << 4 | belt_fl << 3 | belt_fr << 2 | belt_rl << 1 | belt_rr << 0;
-    return res;
+uint32_t uint32Packer(uint32_t speed, uint32_t ocs_fl, uint32_t ocs_fr, uint32_t ocs_rl, uint32_t ocs_rr, uint32_t belt_fl, uint32_t belt_fr, uint32_t belt_rl, uint32_t belt_rr) {
+    uint32_t ret = speed << 24 | ocs_fl << 7 | ocs_fr << 6 | ocs_rl << 5 | ocs_rr << 4 | belt_fl << 3 | belt_fr << 2 | belt_rl << 1 | belt_rr << 0;
+    return ret;
 }
